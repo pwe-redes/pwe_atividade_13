8ad783639c076efb33b58ce1bd0fbcb59c6af494
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2022-11-07 19:24:23 -0300

    Carregar no Github

---
+++ main.py
@@ -0,0 +1,52 @@
+import logging
+import traceback
+
+import flask
+from flask import redirect
+from replit import db
+
+app = flask.Flask(__name__)
+
+@app.errorhandler(500)
+def internal_server_error(e: str):
+    return flask.jsonify(error=str(e)), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def cadastroContatos():
+  try:
+    contatos = db.get('contatos', {});     
+    if flask.request.method == "POST":      
+      contatos[flask.request.form['email']] = {'nome': flask.request.form['nome'],
+                                              'telefone': flask.request.form['telefone'],
+                                              'assunto': flask.request.form['assunto'],
+                                              'mensagem': flask.request.form['mensagem'],
+                                              'resposta': flask.request.form['resposta']}
+    db['contatos'] = contatos
+    return flask.render_template('contatos.html', contatos=contatos)
+  except Exception as e:
+    logging.exception('failed to database')
+    flask.abort(500, description=str(e) + ': ' + traceback.format_exc())
+
+@app.route('/limparBanco', methods=['POST'])
+def limparBanco():
+  try:
+    del db["contatos"];
+    return flask.render_template('contatos.html')
+  except Exception as e:
+    logging.exception(e)
+    return flask.render_template('contatos.html')
+
+@app.route('/delete/<email>', methods=['POST'])
+def delete(email):
+    try:      
+      contatos = db.get('contatos', {});  
+      del contatos[email];
+      db['contatos'] = contatos;
+      return redirect('/');
+    except Exception as e:
+      logging.exception(e)
+      return flask.render_template('contatos.html')
+    
+    
+app.run('0.0.0.0')
